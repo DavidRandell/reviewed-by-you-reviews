8af9a9d7c2fa57a25fd28c1266e9e9ca632bee66
David Randell <david@davidrandell.com>
2022-09-15 21:09:45 +0100

    This post submmission push is to fix an API I was using, their service appears to have stopped

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -4,6 +4,16 @@ const server = require('../app');
 const should = chai.should();
 const Review = require('../models/review');
 
+// Sample DB Data for Testing
+
+const sampleReview =     {
+  "title": "My All Time Favourite Movie",
+  "movie-title": "Dances With Wolves",
+  "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+}
+
+//
+
 chai.use(chaiHttp);
 
 describe('Reviews', ()  => {
@@ -14,12 +24,38 @@ describe('Reviews', ()  => {
         .get('/')
         .end((err, res) => {
           res.should.have.status(200);
+          //res.should.have.status(404);
           res.should.be.html;
           done();
         });
   });
 
   // TEST NEW
+  it('should display new form on /reviews/new GET', (done) => {
+    chai.request(server)
+      .get(`/reviews/new`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html
+          done();
+        });
+  });
+
+
+  // TEST SHOW
+  it('should show a SINGLE review on /reviews/<id> GET', (done) => {
+    var review = new Review(sampleReview);
+    review.save((err, data) => {
+      chai.request(server)
+        .get(`/reviews/${data._id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html
+          done();
+        });
+    });
+  });
+  
   // TEST CREATE
   // TEST SHOW
   // TEST EDIT
